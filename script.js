fcc23aff61126c590bc3627d001aa725c3b9cb97
Possum <darkboy612@gmail.com>
2022-10-12 16:33:57 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+function bookCount() { //Changes placeholder of input to have the amount of books
+	var bookCount = document.getElementById('bookTable').rows.length - 1;
+	document.getElementById('search').placeholder="Search " + bookCount + " books...";
+	return bookCount;
+}
+
+function mergeRow(tr) { //Merges rows with the same author name
+	var cell;
+	var headCell = null;
+	for (var i = 1; i < tr.length; i++) { 
+		cell = tr[i].getElementsByTagName("td")[0];
+		if (headCell == null || cell.innerText !== headCell.innerText) { 
+			headCell = cell; //Keeps track of the upmost cell
+		} else { //Merges cells to one
+			headCell.rowSpan++;
+			cell.style.display = "none"; //Latter cells are not destroyed, but not displayed
+		}
+	}
+}
+
+function unmergeRow(i) { //Gets rid of rowspans and redisplays the given cell
+	cell = tr[i].getElementsByTagName("td")[0];
+	cell.rowSpan = 1;
+	cell.style.display = "";
+}
+
+function filterBooks(tr) { //Filters the table according to book name
+	var search, filter, td, txtValue;
+	search = document.getElementById("search");
+	filter = search.value.toUpperCase(); 
+	for (var i = 0; i < tr.length; i++) {
+		td = tr[i].getElementsByTagName("td")[1];
+		if (td) { //If td exists
+			txtValue = td.innerText || td.textContent;
+			if (txtValue.toUpperCase().indexOf(filter) > -1) { //The indexOf() returns a -1 if a search is not found
+				tr[i].style.display = ""; //Keeps row the way it is
+				unmergeRow(i);
+			} else {
+				tr[i].style.display = "none"; //Hides the entire row
+			}	
+		}
+	}
+
+	if (filter == "") { //If the input has no text in it, reset table
+		mergeRow();
+	}
+}
